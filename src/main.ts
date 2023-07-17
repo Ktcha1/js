88fb2deed5f7a634c8c7a6abe3a90e561674b31b
Ktcha <Ktcha1234556@gmail.com>
2023-07-17 21:14:30 +0300

    js

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
-import './style.css'
-// alert('Котюха')
-console.log('Котюха 2')   
 
-let admin
-let name= 'Джон'
-admin=name
-alert(admin)
+
+
+// 1.Запросите у пользователя число, возведите это число во 2-ю степень и выведите на экран.
+// let input=parseFloat(prompt('Введите число') as string)
+// alert(input*2)
+
+// 2.Запросите у пользователя 2 числа и выведите среднее арифметическое этих чисел
+// let input=parseFloat(prompt('Введите число') as string)
+// let input2=parseFloat(prompt('Введите число') as string)
+// alert((input+input2)/2)
+
+// 3.Запросите у пользователя длину стороны квадрата и выведите площадь такого квадрата.
+// let input=parseFloat(prompt('Введите число') as string)
+// let input2=parseFloat(prompt('Введите число') as string)
+// alert(input*input2)
+
+// 4.Реализуйте конвертор из километров в мили (пользователь вводит километры, программа выводит мили). 1 км = 0,621371 миль. Это значение укажите в коде как константу.
+// let input=parseFloat(prompt('Введите число') as string)
+// const MILKM=0.621371
+// alert(`${input*MILKM}`)
+
+
+// 5.Реализуйте калькулятор. Пользователь вводит два числа,а программа выводит результаты действий + - * / междуэтими числами.
+// let input=parseFloat(prompt('Введите число') as string)
+// let input2=parseFloat(prompt('Введите число') as string)
+// alert(`sum ${input+input2}
+// минус  ${input-input2}
+// уможение ${input*input2}`)
+
+// 6.Пользователь вводит значения a и b для формулы a * x + b = 0,а программа считает и выводит значение x.
+// let a=parseFloat(prompt('Введите a') as string)
+// let b=parseFloat(prompt('Введите b') as string)
+// alert(`${-b/a}`)
+
+// 7.Запросите у пользователя текущее время (часы и минуты)и выведите, сколько часов и минут осталось до следующегодня.
+// let input=parseFloat(prompt('текущий час') as string)
+// let input2=parseFloat(prompt('текущие минуты') as string)
+// let h=parseInt(`${(1440-input*60-input2)/60}`)
+// alert(`до следующего дня осталось ${h} часов ${60-(input2)} минут`)
